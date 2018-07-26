36cd3b223670f6e087c42752c4ea6ae2fc22234a
Jack <151611438@qq.com>
2018-07-26 17:05:06 +0800

    init

--- gifDemo/ViewController.m
+++ gifDemo/ViewController.m
@@ -10,20 +10,51 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //1.加载Gif图片，转换成Data类型
+    NSString *path = [NSBundle.mainBundle pathForResource:@"demo" ofType:@"gif"];
+    NSData *data = [NSData dataWithContentsOfFile:path];
+    
+    //2.将data数据转换成CGImageSource对象
+    CGImageSourceRef imageSource = CGImageSourceCreateWithData(CFBridgingRetain(data), nil);
+    size_t imageCount = CGImageSourceGetCount(imageSource);
+    
+    //3.遍历所有图片
+    NSMutableArray *images = [NSMutableArray array];
+    NSTimeInterval totalDuration = 0;
+    for (int i = 0; i<imageCount; i++) {
+        //取出每一张图片
+        CGImageRef cgImage = CGImageSourceCreateImageAtIndex(imageSource, i, nil);
+        UIImage *image = [UIImage imageWithCGImage:cgImage];
+        [images addObject:image];
+        
+        //持续时间
+        NSDictionary *properties = (__bridge_transfer  NSDictionary*)CGImageSourceCopyPropertiesAtIndex(imageSource, i, nil);
+        NSDictionary *gifDict = [properties objectForKey:(__bridge NSString *)kCGImagePropertyGIFDictionary];
+        NSNumber *frameDuration =
+        [gifDict objectForKey:(__bridge NSString *)kCGImagePropertyGIFDelayTime];
+        totalDuration += frameDuration.doubleValue;
+    }
+    
+    //4.设置imageView属性
+    self.imageView.animationImages = images;
+    self.imageView.animationDuration = totalDuration;
+    self.imageView.animationRepeatCount = 0;
+    
+    //5.开始播放
+    [self.imageView startAnimating];
+    
+    
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 
 @end
